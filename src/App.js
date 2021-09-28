78d8aacb9c7813638bd2e8a8e0c28e2f4c19c481
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-09-28 14:58:22 +0700

    formatting fixes and misc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import './App.css';
-import DrawerToggleButton from './components/SideDrawer/DrawerToggleButton';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import StudyScreen from './components/StudyScreen/StudyScreen';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
+import DrawerToggleButton from './components/SideDrawer/DrawerToggleButton'
+import SideDrawer from './components/SideDrawer/SideDrawer'
+import StudyScreen from './components/StudyScreen/StudyScreen'
 // import SearchScreen from './components/SearchScreen/SearchScreen';
-import TimeLogScreen from './components/TimeLogScreen/TimeLogScreen';
+import TimeLogScreen from './components/TimeLogScreen/TimeLogScreen'
+// import LOGIN SCREEN
 
 //firebase
 // import { DB_CONFIG } from './config/config';
 // import firebase from 'firebase/app';
-// import 'firebase/database'; 
-
+// import 'firebase/database';
 
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props)
 
     // if you don't include the "if statement" an error might occur!
@@ -29,19 +28,16 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount(){
-
+  componentDidMount() {
     //uncomment below to to set data to hardcoded JSON data
     // this.setState({
     //   cards: data["flashcards"],
     // })
-
     //retrieving the data from firebase
     // const database = firebase.database()
     // database.ref('flashcards').on("value", (snapshot) => {
     //   // console.log(snapshot.val())
     //   const firebaseData = snapshot.val()
-
     //   this.setState({
     //     cards: firebaseData
     //   })
@@ -58,28 +54,25 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App">
-          <DrawerToggleButton click={this.drawerToggleClickHandler}/>
-          <SideDrawer show={this.state.sideDrawerOpen}/>
+          <DrawerToggleButton click={this.drawerToggleClickHandler} />
+          <SideDrawer show={this.state.sideDrawerOpen} />
 
-          <Route 
-            path="/" 
-            exact 
-            render={(props) => <StudyScreen {...props} cards={this.state.cards} />}
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <StudyScreen {...props} cards={this.state.cards} />
+            )}
           />
           {/* <Route 
             path="/search" 
             render={(props) => <SearchScreen {...props} cards={this.state.cards} />}
           /> */}
-          <Route 
-            path="/timelog" 
-            render={(props) => <TimeLogScreen/>}
-          />
-  
+          <Route path="/timelog" render={(props) => <TimeLogScreen />} />
         </div>
       </Router>
-    );
-  };
-  
+    )
+  }
 }
 
-export default App;
+export default App
