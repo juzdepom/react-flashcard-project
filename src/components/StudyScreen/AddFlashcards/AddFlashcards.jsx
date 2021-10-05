66b9efa25d52c70bd35f15962b0de1825d82f20c
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-10-05 16:58:38 +0700

    able to save bulk flashcard data to firebase with the 'hashtag' node
    use [#lesson4] to apply this hashtag to all lessons

--- src/components/StudyScreen/AddFlashcards/AddFlashcards.jsx
+++ src/components/StudyScreen/AddFlashcards/AddFlashcards.jsx
@@ -13,6 +13,7 @@ class AddFlashcards extends Component {
             splitTwo: "==",
             splitThree: ">",
             addingCards: false,
+            globalHashtag: "",
         }
 
         this.checkForDuplicatesWithOriginalDeck = this.checkForDuplicatesWithOriginalDeck.bind(this);
@@ -30,22 +31,52 @@ class AddFlashcards extends Component {
         // console.log('testing: ', this.state.originalDeck)
         var flashcardArray = []
         let { text, splitOne, splitTwo, splitThree } = this.state
+
+        //check if there is a global hashtag setting
+        // if(text.includes("#")){
+        //     alert(`Hashtag detected!`)
+        //     this.setState({
+        //         addHashtag: true,
+        //     })
+        // }
         
         //if there is no "splitOne" that means that is either only one flashcard,
         //or you are inputting text that cannot be turned into a flashcard
         if(!text.includes(splitOne)){
+            if(text.startsWith('[#')){
+                //this is not an error
+                return;
+            }
             if(!text.includes(splitTwo) || !text.includes(splitThree)){
                 //string is not valid
                 alert(`Error! The string you input is invalid for flashcard creation`);
                 return;
-            } else {
-                //you have input one flashcard
-                let dict = this.turnStringIntoFlashcard(text, splitTwo, splitThree)
-                flashcardArray.push(dict)
             }
+
+            //you have input one flashcard
+            // let dict = this.turnStringIntoFlashcard(text, splitTwo, splitThree)
+            // flashcardArray.push(dict)
         } else {
+            //check for hashtag
+            // console.log(`I AM HERE ${text}`)
+            // if(text.startsWith("[#")){
+            //     alert('it starts with [#')
+            // }
             var lines = text.split(splitOne)
-        
+            let hashtag = ""
+            if(lines[0].startsWith("[#")){ // APPLY A GLOBAL HASHTAG TO ALL THE FLASHCARDS
+                hashtag = lines[0]
+                hashtag = hashtag.replace('[','').replace(']','')
+                // this.setState({
+                //     globalHashtag: hashtag,
+                // })
+                alert(`hashtag detected! ${hashtag}`)
+                // SET THE STATE TO HASHATG
+                // 
+                // REMOVE THE FIRST ITEM
+                lines.shift()
+            }
+            console.log(lines)
             //go through each flashcard line
             for (var i in lines){
                 var line = lines[i]
@@ -54,11 +85,16 @@ class AddFlashcards extends Component {
                     alert(`Error! Line ${i} is invalid for flashcard creation: ${line} `);
                     return;
                 }
-                let dict = this.turnStringIntoFlashcard(line, splitTwo, splitThree)
+                let dict = this.turnStringIntoFlashcard(line, splitTwo, splitThree, hashtag)
                 flashcardArray.push(dict)
             }
         }
         this.checkForDuplicatesWithOriginalDeck(flashcardArray)
+        
+        
+
+
+        //if 
         
         console.log(`New Flashcard Array: `, flashcardArray)
         this.setState({
@@ -68,7 +104,7 @@ class AddFlashcards extends Component {
 
     }
 
-    turnStringIntoFlashcard(text, splitTwo, splitThree){
+    turnStringIntoFlashcard(text, splitTwo, splitThree, hashtag){
         var dict = {}
         let frontBack = text.split(splitTwo)
         let textOne = frontBack[0].trim()
@@ -85,6 +121,12 @@ class AddFlashcards extends Component {
         dict["dateCreated"] = this.getCurrentDate()
         //add uniqueish id
         dict["_id"] = dict["dateCreated"] + "-" + dict["textOne"]
+        //check if we need to add a global hashtag
+        // const hashtag = this.state.globalHashtag
+        if(hashtag !== ""){
+            console.log(`adding hashtags to the flashcard ${hashtag}`)
+            dict["hashtag"] = hashtag
+        }
 
         if(textOne === "" || textTwo === "" || textThree === ""){
             alert("Warning! This line will have an empty text field: ", dict)
